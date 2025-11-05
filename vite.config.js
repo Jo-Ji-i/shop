0c950451c0d84e39b0b358ec81f8b6b8c536922a
jimmiS2 <gyfl23242@naver.com>
2025-11-05 23:30:40 +0900

    fix: 라우팅 변경 2차

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    routes: [{ src: '/[^.]+', dest: '/', status: 200 }],
+    base: './', // 상대 경로로 변경 → 정적 배포 시 깨짐 방지
 });
